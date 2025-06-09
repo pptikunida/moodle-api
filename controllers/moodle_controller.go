b4db2603951c6513cb88a4794cdf9a50d88c84fa
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-06-09 19:26:20 +0700

    add CreateUser in MoodleController

--- controllers/moodle_controller.go
+++ controllers/moodle_controller.go
@@ -31,3 +31,34 @@ func (s *MoodleController) CheckStatus(c *gin.Context) {
 		Data:    result,
 	})
 }
+
+func (s *MoodleController) CreateUser(c *gin.Context) {
+	var req web.MoodleUserCreateRequest
+
+	// Bind JSON
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, web.ApiResponse{
+			Code:    "INVALID_PARAMS",
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	// Call service
+	result, err := s.moodleService.CreateUser(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, web.ApiResponse{
+			Code:    "INTERNAL_SERVER_ERROR",
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, web.ApiResponse{
+		Code:    "OK",
+		Message: "OK",
+		Data:    result,
+	})
+}
