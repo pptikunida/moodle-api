8b26a5726967f701f1a9ef60062379f96cb4112d
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-06-09 11:04:36 +0700

    add CheckStatus Service

--- services/moodle_service.go
+++ services/moodle_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/rizkycahyono97/moodle-api/model/web"
+
+type MoodleService interface {
+	CheckStatus() (*web.MoodleStatusResponse, error)
+}
--- services/moodle_service_impl.go
+++ services/moodle_service_impl.go
@@ -1,1 +1,62 @@
 package services
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/rizkycahyono97/moodle-api/config"
+	"github.com/rizkycahyono97/moodle-api/model/web"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type MoodleServiceImpl struct {
+	client *http.Client
+}
+
+func NewMoodleService(client *http.Client) MoodleService {
+	return &MoodleServiceImpl{
+		client: client,
+	}
+}
+
+func (s *MoodleServiceImpl) CheckStatus() (*web.MoodleStatusResponse, error) {
+	// Load Env
+	moodleURL := config.GetEnv("MOODLE_URL", "")
+	token := config.GetEnv("MOODLE_TOKEN", "")
+
+	if moodleURL == "" && token == "" {
+		return nil, errors.New("Moodle URL or Token is Empty")
+	}
+
+	// Data Request
+	form := url.Values{}
+	form.Set("token", token)
+	form.Set("wsfunction", "core_webservice_get_site_info")
+	form.Set("moodlewsrestformat", "json")
+
+	// Kirim POST request ke Moodle
+	resp, err := s.client.PostForm(moodleURL, form
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read Response Body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Status Code Check
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("Failed to check Status: " + string(body))
+	}
+
+	// Unmarshal JSON ke struct MoodleStatusResponse
+	var status web.MoodleStatusResponse
+	if err := json.Unmarshal(body, &status); err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
