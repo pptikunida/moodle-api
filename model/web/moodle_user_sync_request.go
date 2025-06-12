fda8ceab0ee7f2c530e8edab3dcc9ae9ffba4d8a
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-06-12 10:54:31 +0700

    modified moodle_user_sync_request

--- model/web/moodle_user_sync_request.go
+++ model/web/moodle_user_sync_request.go
@@ -6,5 +6,27 @@ type MoodleUserSyncRequest struct {
 	FirstName string `json:"first_name" validate:"required"`
 	LastName  string `json:"last_name" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
-	NIM       string `json:"nim" validate:"required"`
+	NIM       string `json:"NIM" validate:"required"`
+
+	// tambahan
+	Auth         string `json:"auth,omitempty"`
+	City         string `json:"city,omitempty"`
+	Country      string `json:"country,omitempty"`
+	Timezone     string `json:"timezone,omitempty"`
+	Description  string `json:"description,omitempty"`
+	Lang         string `json:"lang,omitempty"`
+	Calendartype string `json:"calendartype,omitempty"`
+
+	CustomFields []MoodleUserSyncCustomField `json:"custom_fields,omitempty"`
+	Preferences  []MoodleUserSyncPreference  `json:"preferences,omitempty"`
+}
+
+type MoodleUserSyncCustomField struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type MoodleUserSyncPreference struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
 }
